53ce880a710b6634cbaf78dd457191ba7bd80fe7
Aditya Upadhye <adityasu12@gmail.com>
2024-10-19 16:25:58 +0530

    import change

--- api/messapp/viewsets/foodmenu_viewsets.py
+++ api/messapp/viewsets/foodmenu_viewsets.py
@@ -7,7 +7,7 @@ from rest_framework.decorators import action
 from rest_framework.response import Response
 from rest_framework import status
 from django.utils import timezone
-from datetime import timedelta
+from datetime import timedelta, datetime
 from django.db.models import Avg
 
 
