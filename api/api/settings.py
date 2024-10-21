c072de32b0e24e33a33d8a8f133b1d1a9e092a48
Aditya Upadhye <adityasu12@gmail.com>
2024-10-21 13:53:14 +0530

    changed default database to mysql

--- api/api/settings.py
+++ api/api/settings.py
@@ -80,9 +80,17 @@ WSGI_APPLICATION = 'api.wsgi.application'
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
 DATABASES = {
+    # 'default': {
+    #     'ENGINE': 'django.db.backends.sqlite3',
+    #     'NAME': BASE_DIR / 'db.sqlite3',
+    # },
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'mess_manager_prod',
+        'USER': 'root',
+        'PASSWORD': 'manager',
+        'HOST': '10.129.131.148',  # Or the IP address of your MySQL server
+        'PORT': '3306',       # Default MySQL port
     }
 }
 
