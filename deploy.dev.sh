bf23c97da34cec7d465a68133c14b78c37784e38
Aditya Upadhye <adityasu12@gmail.com>
2024-11-08 15:20:34 +0530

    added mysql docker config

--- deploy.dev.sh
+++ deploy.dev.sh
@@ -14,3 +14,20 @@ docker build -t mess_app .
 
 # run new container
 docker run -d -p 8080:8080 --name mess_app_server mess_app
+
+# run the mysql container if not running
+mysql_container_name="mess_manager_mysql"
+
+if docker ps --filter "name=$mysql_container_name" --filter "status=running" | grep -q "$mysql_container_name"; then
+    echo "Container $mysql_container_name is running."
+else
+    echo "Container $mysql_container_name is not running."
+    docker run -d \
+    --name $mysql_container_name \
+    -e MYSQL_ROOT_PASSWORD=manager \
+    -v mess_manager_data:/var/lib/mysql \
+    -p 3306:3306 \
+    mysql:latest
+
+fi
+
