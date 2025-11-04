96bd72238ceca7f243bfdedb5c301609acf0c015
Aditya Upadhye <adityasu12@gmail.com>
2025-11-05 00:00:39 +0530

    add logger to mgmt commands

--- messapp/management/commands/activate_rebates.py
+++ messapp/management/commands/activate_rebates.py
@@ -1,15 +1,19 @@
 from django.core.management.base import BaseCommand
 from django.utils import timezone
 from messapp.models import MessRebates
+import logging
+
+logger = logging.getLogger(__name__)
 
 class Command(BaseCommand):
     help = 'Updates MessRebates status to expired if end_date has passed and status is active'
 
     def handle(self, *args, **kwargs):
         today = timezone.now().date()
-        print(f'today is {today}')
+        logger.info(f'today is {today}')
         # filter records for inactive status and make then active based on the date condition
         inactive_rebates = MessRebates.objects.filter(status="inactive", end_date__gte=today, start_date__lte=today)
         count = inactive_rebates.update(status="active")
 
+        logger.info(f'{count} records updated to active status.')
         self.stdout.write(f'{count} records updated to active status.')
--- messapp/management/commands/expire_rebates.py
+++ messapp/management/commands/expire_rebates.py
@@ -2,14 +2,18 @@ from django.core.management.base import BaseCommand
 from django.utils import timezone
 from messapp.models import MessRebates
 
+import logging
+logger = logging.getLogger(__name__)
+
 class Command(BaseCommand):
     help = 'Updates MessRebates status to expired if end_date has passed and status is active'
 
     def handle(self, *args, **kwargs):
         today = timezone.now().date()
-        print(f'today is {today}')
+        logger.info(f'today is {today}')
         # Filter records where status is "active" and end_date is before today
         expired_rebates = MessRebates.objects.filter(status="active", end_date__lt=today)
         count = expired_rebates.update(status="expired")
 
+        logger.info(f'{count} records updated to expired status.')
         self.stdout.write(f'{count} records updated to expired status.')
